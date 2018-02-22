bfea1997d8fe68ecdc809a6aebbf31fdf2461dea
Wojciech Kaczmarek <wojtekk@kofeina.net>
2018-02-22 16:17:00 +0100

    debug: print node type mnemonics like TEXT,ELEM

--- debug/print.go
+++ debug/print.go
@@ -7,12 +7,21 @@ import (
 	"golang.org/x/net/html"
 )
 
+var nodeTypes = map[html.NodeType]string{
+	html.ErrorNode:    "ERR",
+	html.DocumentNode: "DOC",
+	html.DoctypeNode:  "DOCTYPE",
+	html.ElementNode:  "ELEM",
+	html.TextNode:     "TEXT",
+	html.CommentNode:  "COMMENT",
+}
+
 func PrintHTML(top *html.Node) {
 	var f func(*html.Node, int)
 
 	f = func(node *html.Node, i int) {
-		fmt.Printf("%sT:%d D:`%s` A:%q\n", strings.Repeat(" ", i*2),
-			node.Type, node.Data, node.Attr)
+		fmt.Printf("%sT:%s D:`%s` A:%q\n", strings.Repeat(" ", i*2),
+			nodeTypes[node.Type], node.Data, node.Attr)
 
 		for c := node.FirstChild; c != nil; c = c.NextSibling {
 			f(c, i+1)
